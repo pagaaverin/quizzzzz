d7ade2c004d961c83172bc99f0ae70c2e73a8024
Paga <pagaaverin@gmail.com>
2024-05-31 13:15:45 +0300

    invest

--- models.js
+++ models.js
@@ -10,3 +10,11 @@ const questionCostails = fs
   .readFileSync('./topics/coctailsquestion.txt', 'utf-8')
   .split('\n');
   
+  const AnswerInvest = fs
+  .readFileSync('./topics/ответы.txt', 'utf-8')
+  .split('\n')
+  .map((el) => el.split(','));
+
+const questionInvest = fs
+  .readFileSync('./topics/вопросы.txt', 'utf-8')
+  .split('\n');
